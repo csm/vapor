e6f42afad33d83afaa4a45f5cf4746f32d7beb39
Casey Marshall <cmarshall@lyveminds.com>
2015-08-05 15:19:29 -0700

    Restructuring.

--- modules/vapor-aws/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to vapor-aws
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.google.guava/guava "18.0"]
+                 [org.apache.sshd/sshd-core "0.14.0"]
+                 [org.bouncycastle/bcprov-jdk15on "1.52"]
+                 [org.bouncycastle/bcpkix-jdk15on "1.52"]]
   :main ^:skip-aot vapor.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}},
-  :plugins [[lein-modules "0.3.11"]],
-  :modules {:dirs ["modules/vapor-aws"]
-            :subprocess nil})
+  :profiles {:uberjar {:aot :all}}
+  :java-source-paths ["javasrc"])
